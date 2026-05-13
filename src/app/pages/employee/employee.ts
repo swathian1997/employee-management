076b5581394c92af214f254a281d5eb07536393b
Swathi <swathian1997@gmail.com>
2026-05-13 20:53:45 +0530

    Added CRUD operations and search

--- src/app/pages/employee/employee.ts
+++ src/app/pages/employee/employee.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-employee',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule, MatButtonModule, FormsModule],
   templateUrl: './employee.html',
   styleUrls: ['./employee.css']
 })
 export class Employee {
+
+  selectedEmployee: any = null;
+  searchText: string = '';
+  displayedColumns: string[] = ['id', 'name', 'email', 'department', 'action'];
+  newEmployee = {
+    id: 0,
+    name: '',
+    email: '',
+    department: ''
+  };
 
   employees = [
     {
@@ -31,4 +44,64 @@ export class Employee {
     }
   ];
 
+  deleteEmployee(id: number) {
+    this.employees = this.employees.filter(
+      emp => emp.id !== id
+    );
+  }
+
+  addEmployee() {
+    this.employees = [...this.employees, 
+      { ...this.newEmployee }
+    ];
+    this.newEmployee = {
+      id: 0,
+      name: '',
+      email: '',
+      department: ''
+    };
+  }
+
+  editEmployee(emp: any) {
+    this.selectedEmployee = { ...emp };
+  }
+
+  updatedEmployee() {
+    const index = this.employees.findIndex(
+      e => e.id === this.selectedEmployee.id
+    );
+    if (index !== -1) {
+      this.employees[index] = this.selectedEmployee;
+      this.employees = [...this.employees];
+    }
+    this.selectedEmployee = null;
+  }
+
+  updateEmployee() {
+
+  const index = this.employees.findIndex(
+    e => e.id === this.selectedEmployee.id
+  );
+
+  if (index !== -1) {
+
+    this.employees[index] = {
+      ...this.selectedEmployee
+    };
+
+    this.employees = [
+      ...this.employees
+    ];
+
+  }
+
+  this.selectedEmployee = null;
+
+}
+
+get filteredEmployees() {
+  return this.employees.filter(
+    emp => emp.name.toLowerCase().includes(this.searchText.toLowerCase())
+  );
+}
 }
